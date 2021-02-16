889e7adae943145ac964bfd01a43fb9be4eccb05
StevennYau <stevenyau@gmail.com>
2021-02-16 15:59:48 -0500

    created get and post methods

--- src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
+++ src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
@@ -1,8 +1,11 @@
 package com.project.RestaurantReviewer.service;
 
 import com.project.RestaurantReviewer.entity.Business;
+import com.project.RestaurantReviewer.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BusinessService {
@@ -16,12 +19,16 @@ public class BusinessService {
     }
 
     //get
-    public Business getBusinesses(){
+    public List<Business> getBusinesses(){
         return businessRepository.findAll();
     }
 
     public Business getBusiness(int id){
         return businessRepository.findById(id).orElse(null);
+    }
+
+    public Business getBusinessByName(String name){
+        return businessRepository.findByName(name);
     }
 
 }
