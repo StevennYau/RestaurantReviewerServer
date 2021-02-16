42af1b6ec5073d9633c01a43b163801cc15e3ad9
StevennYau <stevenyau@gmail.com>
2021-02-16 14:57:57 -0500

    init commit

---
+++ src/test/java/com/project/RestaurantReviewer/RestaurantReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RestaurantReviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
