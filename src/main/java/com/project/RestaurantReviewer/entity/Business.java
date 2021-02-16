e4e0298170bb6367ef04a47ebccc721471fa9e1c
StevennYau <stevenyau@gmail.com>
2021-02-16 15:15:29 -0500

    updated business class

--- src/main/java/com/project/RestaurantReviewer/entity/Business.java
+++ src/main/java/com/project/RestaurantReviewer/entity/Business.java
@@ -1,4 +1,25 @@
 package com.project.RestaurantReviewer.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Business {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String Address;
+    private String city;
+    private String state;
+    private String postal_code;
+    private int latitude;
+    private int longitude;
+    private int stars;
+    private boolean is_open;
+
 }
