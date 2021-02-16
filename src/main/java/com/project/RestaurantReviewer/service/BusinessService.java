7bf8661758cf18b382d0697a7e1d17106a951aa8
StevennYau <stevenyau@gmail.com>
2021-02-16 15:54:37 -0500

    created business service file

--- src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
+++ src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
@@ -1,8 +1,27 @@
 package com.project.RestaurantReviewer.service;
 
+import com.project.RestaurantReviewer.entity.Business;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BusinessService {
+
+    @Autowired
+    private BusinessRepository businessRepository;
+
+    //post
+    public Business saveBusiness(Business business){
+        return businessRepository.save(business);
+    }
+
+    //get
+    public Business getBusinesses(){
+        return businessRepository.findAll();
+    }
+
+    public Business getBusiness(int id){
+        return businessRepository.findById(id).orElse(null);
+    }
 
 }
