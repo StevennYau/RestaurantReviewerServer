dc0bd6a135f86366d75c5fd31d5fd93212e82ff5
StevennYau <stevenyau@gmail.com>
2021-02-17 15:22:06 -0500

    added query for finding id by username and vice versa

--- src/main/java/com/project/RestaurantReviewer/repository/UserRepository.java
+++ src/main/java/com/project/RestaurantReviewer/repository/UserRepository.java
@@ -2,9 +2,22 @@ package com.project.RestaurantReviewer.repository;
 
 import com.project.RestaurantReviewer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     Optional<User> findByUserName(String userName);
+
+
+    User findUserById(int id);
+
+    @Query(value = "SELECT id FROM User u WHERE u.user_name = :userName",nativeQuery = true)
+    int findIdByUser(@Param("userName") String userName);
+
+
+    /*@Query(value = "SELECT u FROM User u WHERE u.id = :id",nativeQuery = true)
+    User findUserById(@Param("userName") int id);*/
 }
