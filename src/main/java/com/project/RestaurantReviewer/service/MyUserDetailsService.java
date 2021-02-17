49c788e5d94ee9aecb233ffa36164adea3c4bf5a
StevennYau <stevenyau@gmail.com>
2021-02-17 14:36:38 -0500

    update

--- src/main/java/com/project/RestaurantReviewer/service/MyUserDetailsService.java
+++ src/main/java/com/project/RestaurantReviewer/service/MyUserDetailsService.java
@@ -1,18 +1,21 @@
 package com.project.RestaurantReviewer.service;
-import com.project.RestaurantReviewer.MyUserDetails;
+import com.project.RestaurantReviewer.controller.UserController;
+import com.project.RestaurantReviewer.entity.MyUserDetails;
 import com.project.RestaurantReviewer.entity.User;
 import com.project.RestaurantReviewer.repository.UserRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 
@@ -21,6 +24,9 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
+
+    private static Logger log = LoggerFactory.getLogger(UserController.class);
+
 
 
     public String getLoggedInUser() {
@@ -35,17 +41,23 @@ public class MyUserDetailsService implements UserDetailsService {
         return null;
     }
 
-    /*public String getLoggedInId() {
+    public String getLoggedInId(String userName) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         // log.debug("authentication="+authentication);
+        Optional<User> test = userRepository.findIdByUser(userName);
+        log.info("ID ID ID");
+        log.info("ID ID ID");
+        log.info("ID ID ID");
+        log.info("ID ID ID");
+        log.info(String.valueOf(test));
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
 
-            String currentUserName = authentication.getID();
+            String currentUserName = authentication.getName();
             //log.debug("currentUserName="+currentUserName);
             return currentUserName;
         }
         return null;
-    }*/
+    }
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
