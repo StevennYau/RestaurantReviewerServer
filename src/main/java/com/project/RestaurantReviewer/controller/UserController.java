1aa4ecab5fcb656a4aa998243224e8181d8917da
StevennYau <stevenyau@gmail.com>
2021-02-17 14:36:00 -0500

    finding user id from username handler

--- src/main/java/com/project/RestaurantReviewer/controller/UserController.java
+++ src/main/java/com/project/RestaurantReviewer/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.project.RestaurantReviewer.controller;
 
+import com.project.RestaurantReviewer.entity.User;
 import com.project.RestaurantReviewer.service.MyUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,11 +17,21 @@ public class UserController {
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
-    @GetMapping("/getUserTest")
-    public String getUserTest(){
+
+    @GetMapping("/getUserName")
+    public String getUserName(){
         String test = myUserDetailsService.getLoggedInUser();
         System.out.println(test);
         log.info("this is log back");
+
+        return test;
+    }
+
+
+    @GetMapping("/getUserId/{userName}")
+    public String getUserId(@PathVariable String userName){
+        String test = myUserDetailsService.getLoggedInId(userName);
+
         return test;
     }
 
