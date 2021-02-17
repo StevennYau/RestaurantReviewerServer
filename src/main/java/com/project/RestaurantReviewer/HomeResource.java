a6ac76834bf0f4433120acc67e4f922cf140fc8d
StevennYau <stevenyau@gmail.com>
2021-02-16 22:41:45 -0500

    home resource created

---
+++ src/main/java/com/project/RestaurantReviewer/HomeResource.java
@@ -0,0 +1,29 @@
+package com.project.RestaurantReviewer;
+
+import com.project.RestaurantReviewer.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeResource {
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName(User user){
+        return user.getUserName();
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+}
