20ded72a9a5ca6a4b45c9206175bbb4789c20294
StevennYau <stevenyau@gmail.com>
2021-02-18 13:02:13 -0500

    added crud methods

--- src/main/java/com/project/RestaurantReviewer/repository/FavTableRepository.java
+++ src/main/java/com/project/RestaurantReviewer/repository/FavTableRepository.java
@@ -1,5 +1,12 @@
 package com.project.RestaurantReviewer.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;@
+import com.project.RestaurantReviewer.entity.FavTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 public interface FavTableRepository extends JpaRepository<FavTable, Integer> {
+    FavTable findByName(String name);
+
+    /*@Query(value = "DELETE FROM ")
+    void deleteByName(String name);*/
 }
