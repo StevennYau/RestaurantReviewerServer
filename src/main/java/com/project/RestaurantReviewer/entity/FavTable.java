0450c2e7f171987beb915569305bd04e05fd68cb
StevennYau <stevenyau@gmail.com>
2021-02-17 19:14:47 -0500

    added getters and setters for fav table

--- src/main/java/com/project/RestaurantReviewer/entity/FavTable.java
+++ src/main/java/com/project/RestaurantReviewer/entity/FavTable.java
@@ -17,4 +17,35 @@ public class FavTable {
     @Column(name="user_rating")
     private int user_rating;
 
+    public int getFav_id() {
+        return fav_id;
+    }
+
+    public void setFav_id(int fav_id) {
+        this.fav_id = fav_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getBusiness_id() {
+        return business_id;
+    }
+
+    public void setBusiness_id(int business_id) {
+        this.business_id = business_id;
+    }
+
+    public int getUser_rating() {
+        return user_rating;
+    }
+
+    public void setUser_rating(int user_rating) {
+        this.user_rating = user_rating;
+    }
 }
