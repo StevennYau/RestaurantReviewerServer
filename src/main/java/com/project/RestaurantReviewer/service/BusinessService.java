5c0bf17ba540b6865a6aedcae23c351611a3c25c
StevennYau <stevenyau@gmail.com>
2021-02-18 18:13:42 -0500

    added methods to add and delete (cascading delete) businesses along with their address's

--- src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
+++ src/main/java/com/project/RestaurantReviewer/service/BusinessService.java
@@ -1,6 +1,9 @@
 package com.project.RestaurantReviewer.service;
 
+import com.project.RestaurantReviewer.entity.Address;
 import com.project.RestaurantReviewer.entity.Business;
+import com.project.RestaurantReviewer.entity.BusinessInfo;
+import com.project.RestaurantReviewer.repository.AddressRepository;
 import com.project.RestaurantReviewer.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +16,41 @@ public class BusinessService {
     @Autowired
     private BusinessRepository businessRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     //post
     public Business saveBusiness(Business business){
         return businessRepository.save(business);
+    }
+
+    public Business addBusinessWithAddress(BusinessInfo businessInfo){
+        // save address first
+        Address addressInfo = new Address();
+        addressInfo.setLatitude(businessInfo.getLatitude());
+        addressInfo.setLongitude(businessInfo.getLongitude());
+        addressInfo.setAddress1(businessInfo.getAddress1());
+        addressInfo.setAddress2(businessInfo.getAddress2());
+        addressInfo.setAddress3(businessInfo.getAddress3());
+        addressInfo.setCity(businessInfo.getCity());
+        addressInfo.setZip_code(businessInfo.getZip_code());
+        addressInfo.setCountry(businessInfo.getCountry());
+        addressInfo.setState(businessInfo.getState());
+        Address newAddress = addressRepository.save(addressInfo);
+
+        //  addres_id (newAddress.getid) and other info, insert a new Business record
+        Business newBusiness = new Business();
+        newBusiness.setAlias(businessInfo.getAlias());
+        newBusiness.setName(businessInfo.getName());
+        newBusiness.setImage_url(businessInfo.getImage_url());
+        newBusiness.setIs_closed(businessInfo.isIs_closed());
+        newBusiness.setUrl(businessInfo.getUrl());
+        newBusiness.setReview_count(businessInfo.getReview_count());
+        newBusiness.setRating(businessInfo.getRating());
+        newBusiness.setPhone(businessInfo.getPhone());
+        newBusiness.setAddress_id(newAddress.getAddress_id());
+
+        return businessRepository.save(newBusiness);
     }
 
     public List<Business> saveBusinesses(List<Business> businesses){
@@ -39,6 +74,18 @@ public class BusinessService {
     public String deleteBusiness(int id){
         businessRepository.deleteById(id);
         return "business " + id + " has been removed";
+    }
+
+    public String deleteBusinessWithAddress(int id){
+        Business oldBusiness = businessRepository.findById(id).orElse(null);
+        if (oldBusiness == null){
+            return "business " + id + " does not exist";
+        }
+        int addressId = oldBusiness.getAddress_id();
+        addressRepository.deleteById(addressId); // cascading delete
+        businessRepository.deleteById(id);
+
+        return "business " + id + " has been removed and address " + addressId + " has been removed";
     }
 
     //update
