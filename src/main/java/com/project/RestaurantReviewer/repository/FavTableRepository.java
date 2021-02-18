9043354c1f3339dd85c36620ec97ecb8bedfeb9d
StevennYau <stevenyau@gmail.com>
2021-02-17 19:24:38 -0500

    created fav table repositry

---
+++ src/main/java/com/project/RestaurantReviewer/repository/FavTableRepository.java
@@ -0,0 +1,5 @@
+package com.project.RestaurantReviewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;@
+public interface FavTableRepository extends JpaRepository<FavTable, Integer> {
+}
