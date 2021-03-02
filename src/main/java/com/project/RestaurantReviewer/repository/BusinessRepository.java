6842a9cebe851647fb2cdb1350bd78250ec43e5c
StevennYau <stevenyau@gmail.com>
2021-03-01 21:58:41 -0500

    added new sql query to search for businesses that contain a certain string for search bar

--- src/main/java/com/project/RestaurantReviewer/repository/BusinessRepository.java
+++ src/main/java/com/project/RestaurantReviewer/repository/BusinessRepository.java
@@ -3,7 +3,14 @@ package com.project.RestaurantReviewer.repository;
 
 import com.project.RestaurantReviewer.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BusinessRepository extends JpaRepository<Business,Integer> {
-    Business findByName(String name);
+
+    @Query(value = "SELECT * FROM business b WHERE b.name LIKE %:name%", nativeQuery = true)
+    List<Business> findByName(@Param("name") String name);
+
 }
