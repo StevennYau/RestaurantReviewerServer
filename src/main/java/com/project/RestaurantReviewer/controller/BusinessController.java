3ac21716208c5b925d313f0d309d26633ecdf621
StevennYau <stevenyau@gmail.com>
2021-03-01 21:57:34 -0500

    added new method for search bar

--- src/main/java/com/project/RestaurantReviewer/controller/BusinessController.java
+++ src/main/java/com/project/RestaurantReviewer/controller/BusinessController.java
@@ -1,9 +1,14 @@
 package com.project.RestaurantReviewer.controller;
 
-import com.project.RestaurantReviewer.entity.Business;
+import com.project.RestaurantReviewer.entity.*;
 import com.project.RestaurantReviewer.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -24,6 +29,17 @@ public class BusinessController {
         return businessService.saveBusinesses(businesses);
     }
 
+    @PostMapping("/addBusinessWithAddress")
+    public Business addBusinessWithAddress(@RequestBody BusinessInfo businessInfo){
+        return businessService.addBusinessWithAddress(businessInfo);
+    }
+
+    //change to post
+    @GetMapping("/addBusinessFromYelp/{location}/{keyword}/{offset}")
+    public Business addBusinessFromYelp(@PathVariable String location, @PathVariable String keyword, @PathVariable int offset){
+        return businessService.addBusinessFromYelp(location, keyword, offset);
+    }
+
     //get
     @GetMapping("/businesses")
     public List<Business> findAllBusinesses(){
@@ -36,14 +52,25 @@ public class BusinessController {
     }
 
     @GetMapping("/businessByName/{name}")
-    public Business findBusinessByName(@PathVariable String name){
+    public List<Business> findBusinessByName(@PathVariable String name){
         return businessService.getBusinessByName(name);
+    }
+
+    @GetMapping("/getBusinessFromYelpById/{id}")
+    public Mono<YelpBusiness> getBusinessFromYelpById(@PathVariable String id){
+        return businessService.getBusinessFromYelpById(id);
     }
 
     //delete
     @DeleteMapping("/deleteBusiness/{id}")
     public String deleteBusiness(@PathVariable int id){
         return businessService.deleteBusiness(id);
+    }
+
+    @DeleteMapping("/deleteBusinessWithAddress/{id}") //using business id
+    public ResponseEntity<?> deleteBusinessWithAddress(@PathVariable int id){
+        businessService.deleteBusinessWithAddress(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //put
